4270c088bc95e111fe1b1bcb3662cd41840fb4fc
Kim Seon Woo <seonwoo960000@naver.com>
2023-09-30 18:16:47 +0900

    Add feature to open sidePanel when chrome extension icon is clicked

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,14 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: 'openSidePanel',
-    title: 'Open Side Panel',
-    contexts: ['all']
+    id: "openSidePanel",
+    title: "Open Side Panel",
+    contexts: ["all"]
   });
-  chrome.tabs.create({ url: 'html/page.html' });
+  chrome.tabs.create({ url: "html/page.html" });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'openSidePanel') {
+  if (info.menuItemId === "openSidePanel") {
     // This will open the panel in all the pages on the current window.
     chrome.sidePanel.open({ windowId: tab.windowId });
   }
@@ -17,24 +17,31 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 chrome.runtime.onMessage.addListener((message, sender) => {
   // The callback for runtime.onMessage must return falsy if we're not sending a response
   (async () => {
-    if (message.type === 'open_side_panel') {
+    if (message.type === "open_side_panel") {
       // This will open a tab-specific side panel only on the current tab.
       await chrome.sidePanel.open({ tabId: sender.tab.id });
       await chrome.sidePanel.setOptions({
         tabId: sender.tab.id,
-        path: 'index.html',
-        enabled: true
+        path: "index.html",
       });
     }
   })();
 });
 
-
 chrome.commands.onCommand.addListener((command) => {
-  if (command === 'open_sidepanel') {
+  if (command === "open_sidepanel") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const currentTab = tabs[0];
       chrome.sidePanel.open({ windowId: currentTab.windowId });
     });
   }
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  const tabId = tab.id;
+  await chrome.sidePanel.open({ tabId });
+  await chrome.sidePanel.setOptions({
+    tabId,
+    path: "index.html",
+  });
 });
