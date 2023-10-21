0b0dee7e6b901d67fd2e94da50fc6cb7bed342e0
Kim Seon Woo <seonwoo960000@naver.com>
2023-10-22 01:45:44 +0900

    Enhance landing page

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,71 +10,144 @@ const startUsing = "Press \"Command + Shift + O\" for Mac. " +
 function toGithub() {
   window.open("https://github.com/seonwoo960000/ai-sidebar", "_blank");
 }
+
+function toChromeStore() {
+  window.open("https://chromewebstore.google.com/detail/free-ai-side-bar/bphjdepgpbodffelhponjdfpjdajghgc?hl=ko", "_blank");
+}
 </script>
 
 <template>
   <v-container class="background" fluid>
-    <div class="box-1">
-      <v-card class="appbar" elevation="0">
-        <div class="d-flex align-center pa-4 font-weight-bold">
-          <v-img src="@/assets/logo.png"
-                 alt="FREE AI Sidebar"
-                 width="25px"
-                 height="25px" />
-          <span class="ml-4">FREE AI</span>
+    <v-row class="box-1">
+      <v-col cols="12">
+        <v-card class="appbar" elevation="0">
+          <div class="d-flex align-center pa-4 font-weight-bold">
+            <v-img src="@/assets/logo.png"
+                   alt="FREE AI Sidebar"
+                   width="25px"
+                   height="25px" />
+            <span class="ml-4">FREE AI</span>
+          </div>
+          <v-tabs v-model="tab"
+                  align-tabs="center"
+                  bg-color="transparent">
+            <v-tab v-for="tab in tabs"
+                   class="tab text-capitalize mx-2"
+                   hide-slider
+                   :key="tab"
+                   :value="tab">
+              {{ tab }}
+            </v-tab>
+          </v-tabs>
+          <div>
+            <v-btn disabled>
+              NO SIGN UP
+            </v-btn>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <div class="description text-center">
+          <div class="description-1">
+            Introducing Free AI Sidebar
+          </div>
+          <div class="description-2">
+            AI Tools Made Free
+          </div>
+          <div class="description-3">
+            Free AI Sidebar is a free and open source AI tool <br />
+            which is available for everyone to use. <br />
+          </div>
+          <div>
+            <v-tooltip :text="startUsing"
+                       max-width="300px"
+                       location="bottom">
+              <template v-slot:activator="{ props }">
+                <v-btn class="description-button-1 text-capitalize"
+                       size="large"
+                       v-bind="props"
+                >
+                  Start Using
+                </v-btn>
+              </template>
+            </v-tooltip>
+            <v-btn class="description-button-2 text-capitalize"
+                   size="large"
+                   @click="toGithub">
+              <v-icon>
+                mdi-github
+              </v-icon>
+              Github
+            </v-btn>
+          </div>
         </div>
-        <v-tabs v-model="tab"
-                align-tabs="center"
-                bg-color="transparent">
-          <v-tab v-for="tab in tabs"
-                 class="tab text-capitalize mx-2"
-                 hide-slider
-                 :key="tab"
-                 :value="tab">
-            {{ tab }}
-          </v-tab>
-        </v-tabs>
+      </v-col>
+      <v-col cols="12">
+        <span class="theme-images">
+        <v-img class="theme-image"
+               src="@/assets/example-themes.png"
+               height="600" />
+      </span>
+      </v-col>
+    </v-row>
+    <v-row class="box-2">
+      <v-col class="theme-left-column"
+             cols="7">
+        <v-img class="theme-image"
+               src="@/assets/example-message-template.png"
+               max-width="640" />
+      </v-col>
+      <v-col class="template-right-column"
+             cols="5">
         <div>
-          <v-btn disabled>
-            NO SIGN UP
-          </v-btn>
+          <div class="template-title">
+            Configure your template
+          </div>
+          <div class="template-description">
+            You can set your default message template so that you don't have to repeat the same message over and
+            over again!!
+          </div>
         </div>
-      </v-card>
-      <div class="description text-center">
-        <div class="description-1">
-          Introducing Free AI Sidebar
+      </v-col>
+    </v-row>
+    <v-row class="box-3">
+      <v-col class="memorization-left-column"
+             cols="5">
+        <div style="max-width: 420px">
+          <div class="memorization-title">
+            Remember the context
+          </div>
+          <div class="memorization-description">
+            Remember context so that you can seamlessly continue your conversation with chatGPT.
+          </div>
         </div>
-        <div class="description-2">
-          AI Tools Made Free
+      </v-col>
+      <v-col class="memorization-right-column"
+             cols="7">
+        <v-img class="theme-image"
+               src="@/assets/example-memorization.png"
+               max-width="640" />
+      </v-col>
+    </v-row>
+    <v-row class="box-4">
+      <div class="mx-auto">
+        <div class="footer-title">Links</div>
+        <div class="footer-item"
+             @click="toGithub">
+          <v-icon>
+            mdi-github
+          </v-icon>
+          Github
         </div>
-        <div class="description-3">
-          Free AI Sidebar is a free and open source AI tool <br />
-          which is available for everyone to use. <br />
-        </div>
-        <div>
-          <v-tooltip :text="startUsing"
-                     max-width="300px"
-                     location="bottom">
-            <template v-slot:activator="{ props }">
-              <v-btn class="description-button-1 text-capitalize"
-                     size="large"
-                     v-bind="props"
-              >
-                Start Using
-              </v-btn>
-            </template>
-          </v-tooltip>
-          <v-btn class="description-button-2 text-capitalize"
-                 size="large"
-                 @click="toGithub">
-            <v-icon>
-              mdi-github
-            </v-icon>
-            Github
-          </v-btn>
+        <div class="footer-item"
+             @click="toChromeStore">
+          <v-icon>
+            mdi-google-chrome
+          </v-icon>
+          Chrome Extension
         </div>
       </div>
-    </div>
+    </v-row>
   </v-container>
 </template>
 
@@ -86,8 +159,31 @@ function toGithub() {
 
 .box-1 {
   border-radius: 0.8rem;
-  padding: 32px;
+  margin-bottom: 16px;
+  padding: 24px 0 48px 0;
   background-image: linear-gradient(to bottom right, #FBD7FF 10%, #FFDEC1 80%);
+}
+
+.box-2 {
+  border-radius: 0.8rem;
+  margin-bottom: 16px;
+  padding: 96px 0;
+  background-color: #F4F4F4;
+}
+
+.box-3 {
+  border-radius: 0.8rem;
+  margin-bottom: 16px;
+  padding: 96px 0;
+  background-image: linear-gradient(to bottom right, #FBD7FF 10%, #FFDEC1 80%);
+}
+
+.box-4 {
+  border-radius: 0.8rem;
+  padding: 24px 0;
+  background-color: #000000;
+  display: flex;
+  justify-content: center;
 }
 
 .appbar {
@@ -138,5 +234,67 @@ function toGithub() {
   margin: 0 12px;
   border-radius: 0.375rem;
   background-color: rgba(0, 0, 0, 0.06);;
+}
+
+.theme-images {
+  background-color: transparent;
+  border-radius: 1rem;
+}
+
+.theme-image {
+  background-color: transparent;
+  border-radius: 1rem;
+}
+
+.theme-left-column {
+  display: flex;
+  justify-content: end;
+}
+
+.template-title {
+  font-size: 2.5rem;
+  font-weight: 600;
+  margin-bottom: 12px;
+}
+
+.template-description {
+  color: #555555
+}
+
+.template-right-column {
+  max-width: 420px;
+  padding-right: 8px
+}
+
+.memorization-left-column {
+  display: flex;
+  justify-content: end;
+}
+
+.memorization-right-column {
+}
+
+.memorization-title {
+  padding-right: 0;
+  font-size: 2.5rem;
+  font-weight: 600;
+  margin-bottom: 12px;
+}
+
+.memorization-description {
+  color: #555555
+}
+
+.footer-title {
+  color: #FFFFFF;
+  font-weight: bold;
+  margin-bottom: 16px;
+}
+
+.footer-item {
+  cursor: pointer;
+  margin-bottom: 12px;
+  color: #727272;
+  font-size: 0.8rem;
 }
 </style>
