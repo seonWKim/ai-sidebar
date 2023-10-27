544bd500c9727a6ebda7a83e64fa1c8b1ccf7fc5
Kim Seon Woo <seonwoo960000@naver.com>
2023-10-27 16:31:38 +0900

    Fix the sidePanel to be opened as a global tab

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,9 @@
-chrome.tabs
-  .query({
-    active: true,
-    lastFocusedWindow: true,
-  })
-  .then((tabs) => {
-    const tabId = tabs[0].id;
-    const button = document.getElementById('openSidePanel');
-    button.addEventListener('click', async () => {
-      await chrome.sidePanel.open({ tabId });
-      await chrome.sidePanel.setOptions({
-        tabId,
-        path: 'index.html',
-      });
-    });
+chrome.tabs.query({
+  active: true,
+  lastFocusedWindow: true,
+}).then((tabs) => {
+  const button = document.getElementById('openSidePanel');
+  button.addEventListener('click', async () => {
+    await chrome.sidePanel.open({ windowId: tabs[0].windowId });
   });
+});
