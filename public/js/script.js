d32d3469366cb32efb50fc9e4a053069fa68f785
Kim Seon Woo <seonwoo960000@naver.com>
2023-10-27 16:33:00 +0900

    Fix package.json

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,11 @@
-chrome.tabs.query({
-  active: true,
-  lastFocusedWindow: true,
-}).then((tabs) => {
-  const button = document.getElementById('openSidePanel');
-  button.addEventListener('click', async () => {
-    await chrome.sidePanel.open({ windowId: tabs[0].windowId });
+chrome.tabs
+  .query({
+    active: true,
+    lastFocusedWindow: true,
+  })
+  .then((tabs) => {
+    const button = document.getElementById('openSidePanel');
+    button.addEventListener('click', async () => {
+      await chrome.sidePanel.open({ windowId: tabs[0].windowId });
+    });
   });
-});
